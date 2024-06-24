f6edd4d8c482e1cc463db3b900de30574b353bf3
shreyakrishnann <134472993+shreyakrishnann@users.noreply.github.com>
2024-06-24 23:47:57 +0800

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,1 +1,23 @@
+const path = require('path');
 
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'src/build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
