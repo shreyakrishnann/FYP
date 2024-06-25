5f0c1940bc99e553ac60279ac55777adf31ba89c
shreyakrishnann <134472993+shreyakrishnann@users.noreply.github.com>
2024-06-25 18:24:29 +0800

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,20 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Ensure this path matches your directory structure
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
+  mode: 'development', // or 'production'
 };
