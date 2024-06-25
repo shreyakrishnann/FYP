8d68839bea08ac0772bbc300b06f55251b364dca
shreyakrishnann <134472993+shreyakrishnann@users.noreply.github.com>
2024-06-25 19:38:45 +0800

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: '/FYP/src/index.js', // Ensure this path is correct relative to the webpack.config.js file
+  entry: 'C:/GitHub_Repository/FYP/src/index.js', // Ensure this path is correct relative to the webpack.config.js file
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build'), // This should point to the build directory
